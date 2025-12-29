c72923da6475dcd2b6d62bfcc83cab61a07900d0
astra <astra@yandex.ru>
2025-12-29 08:52:48 +0300

    cafateria 3

--- sprint1/problems/cafeteria/solution/src/cafeteria.h
+++ sprint1/problems/cafeteria/solution/src/cafeteria.h
@@ -6,13 +6,14 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/steady_timer.hpp>
 #include <boost/asio/strand.hpp>
+#include <atomic>
 #include <memory>
 
 #include "hotdog.h"
 #include "result.h"
 
 namespace net = boost::asio;
-namespace sys = boost::system; 
+namespace sys = boost::system;
 
 using HotDogHandler = std::function<void(Result<HotDog> hot_dog)>;
 
@@ -22,72 +23,109 @@ public:
         : io_{io} {
     }
 
-    // Асинхронно готовит хот-дог
     void OrderHotDog(HotDogHandler handler) {
+        // Уникальный ID для этого заказа
+        static std::atomic<int> next_hotdog_id{1};
+        int hotdog_id = next_hotdog_id++;
+        
+        // Получаем ингредиенты
         auto sausage = store_.GetSausage();
         auto bread = store_.GetBread();
         
+        // Создаём strand для этого заказа
         auto strand = std::make_shared<net::strand<net::io_context::executor_type>>(
-            net::make_strand(io_));  // Упростил
+            net::make_strand(io_));
         
-        auto counter = std::make_shared<int>(0);
-        auto hotdog_id = std::make_shared<int>(0);
+        // Состояние для этого заказа
+        struct OrderState {
+            bool sausage_ready = false;
+            bool bread_ready = false;
+            std::optional<Result<HotDog>> result;
+            int hotdog_id;
+        };
         
-        auto try_make_hotdog = [strand, handler, counter, hotdog_id, sausage, bread]() {
-            if (*counter == 2) {
+        auto state = std::make_shared<OrderState>();
+        state->hotdog_id = hotdog_id;
+        
+        // Функция для создания хот-дога, когда оба ингредиента готовы
+        auto try_make_hotdog = [strand, handler, state, sausage, bread]() {
+            if (state->sausage_ready && state->bread_ready && !state->result) {
                 try {
                     // Создаём хот-дог
-                    HotDog hot_dog{++(*hotdog_id), sausage, bread};
-                    net::post(*strand, [handler, hot_dog = std::move(hot_dog)]() mutable {
-                        handler(Result<HotDog>{std::move(hot_dog)});
+                    HotDog hot_dog{state->hotdog_id, sausage, bread};
+                    state->result = Result<HotDog>{std::move(hot_dog)};
+                    net::post(*strand, [handler, result = *state->result]() mutable {
+                        handler(std::move(result));
+                    });
+                } catch (const std::exception& e) {
+                    state->result = Result<HotDog>{std::make_exception_ptr(e)};
+                    net::post(*strand, [handler, result = *state->result]() mutable {
+                        handler(std::move(result));
                     });
                 } catch (...) {
-                    net::post(*strand, [handler]() {
-                        handler(Result<HotDog>::FromCurrentException());
+                    state->result = Result<HotDog>::FromCurrentException();
+                    net::post(*strand, [handler, result = *state->result]() mutable {
+                        handler(std::move(result));
                     });
                 }
             }
         };
         
         // Готовим сосиску
-        sausage->StartFry(*gas_cooker_, [sausage, strand, counter, try_make_hotdog]() {
-            auto timer = std::make_shared<net::steady_timer>(*strand);
-            timer->expires_after(std::chrono::milliseconds(1500));
-            
-            timer->async_wait([sausage, strand, counter, try_make_hotdog, timer]
-                            (const sys::error_code& ec) {  
-                if (ec) return;
+        try {
+            sausage->StartFry(*gas_cooker_, [sausage, strand, state, try_make_hotdog]() {
+                auto timer = std::make_shared<net::steady_timer>(*strand);
+                timer->expires_after(std::chrono::milliseconds(1500));
                 
-                try {
-                    sausage->StopFry();
-                    (*counter)++;
-                    net::post(*strand, try_make_hotdog);
-                } catch (...) {
-                    (*counter)++;
-                    net::post(*strand, try_make_hotdog);
-                }
+                timer->async_wait([sausage, strand, state, try_make_hotdog, timer]
+                                (const sys::error_code& ec) {
+                    if (ec) return;
+                    
+                    try {
+                        sausage->StopFry();
+                        state->sausage_ready = true;
+                        net::post(*strand, try_make_hotdog);
+                    } catch (...) {
+                        state->sausage_ready = true;
+                        net::post(*strand, try_make_hotdog);
+                    }
+                });
             });
-        });
+        } catch (...) {
+            // Если не удалось начать готовить сосиску, сразу возвращаем ошибку
+            net::post(*strand, [handler]() {
+                handler(Result<HotDog>::FromCurrentException());
+            });
+            return;
+        }
         
         // Готовим булку
-        bread->StartBake(*gas_cooker_, [bread, strand, counter, try_make_hotdog]() {
-            auto timer = std::make_shared<net::steady_timer>(*strand);
-            timer->expires_after(std::chrono::milliseconds(1000));
-            
-            timer->async_wait([bread, strand, counter, try_make_hotdog, timer]
-                            (const sys::error_code& ec) {  
-                if (ec) return;
+        try {
+            bread->StartBake(*gas_cooker_, [bread, strand, state, try_make_hotdog]() {
+                auto timer = std::make_shared<net::steady_timer>(*strand);
+                timer->expires_after(std::chrono::milliseconds(1000));
                 
-                try {
-                    bread->StopBaking();
-                    (*counter)++;
-                    net::post(*strand, try_make_hotdog);
-                } catch (...) {
-                    (*counter)++;
-                    net::post(*strand, try_make_hotdog);
-                }
+                timer->async_wait([bread, strand, state, try_make_hotdog, timer]
+                                (const sys::error_code& ec) {
+                    if (ec) return;
+                    
+                    try {
+                        bread->StopBaking();
+                        state->bread_ready = true;
+                        net::post(*strand, try_make_hotdog);
+                    } catch (...) {
+                        state->bread_ready = true;
+                        net::post(*strand, try_make_hotdog);
+                    }
+                });
             });
-        });
+        } catch (...) {
+            // Если не удалось начать готовить булку, нужно попытаться освободить сосиску
+            net::post(*strand, [handler]() {
+                handler(Result<HotDog>::FromCurrentException());
+            });
+            return;
+        }
     }
 
 private:
